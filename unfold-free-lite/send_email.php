a3bc8033a66244546063d1196e62c71896c61d02
arianneghislainerull <arianneghislaine@gmail.com>
2023-05-09 16:08:03 -0400

    Update send_email.php

--- unfold-free-lite/send_email.php
+++ unfold-free-lite/send_email.php
@@ -1,29 +1,20 @@
 <?php
-// Load PHPMailer
-require 'phpmailer/PHPMailerAutoload.php';
-
-// Instantiate a new PHPMailer object
-$mail = new PHPMailer;
-
-// Configure SMTP settings
-$mail->isSMTP();
-$mail->Host = 'smtp.example.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'your_username';
-$mail->Password = 'your_password';
-$mail->SMTPSecure = 'tls';
-$mail->Port = 587;
-
-// Set email content
-$mail->setFrom($_POST['email'], $_POST['name']);
-$mail->addAddress('arianneghislaine@gmail.com');
-$mail->Subject = 'New message from website contact form';
-$mail->Body = $_POST['message'];
-
-// Send email and check for errors
-if (!$mail->send()) {
-    echo 'Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "arianneghislaine@gmail.com";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $headers = "From: $name <$email>";
+    
+    if (mail($to, "Message from $name", $message, $headers)) {
+        $status = "success";
+        $response = "Thank you for your message!";
+    } else {
+        $status = "error";
+        $response = "Oops! Something went wrong, please try again later.";
+    }
+    header("Location: index.php?status=$status&response=$response");
+    exit();
 }
 ?>
+
